8fe8522d56c11646d3cca26c1f1665b16195da16
Edi Simetzberger <simetzberger@gmail.com>
2023-03-25 23:44:58 +0100

    callback oauth

--- api/callback.ts
+++ api/callback.ts
@@ -1,10 +1,50 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { create, renderBody } from "./_lib/oauth2";
+// import { create, renderBody } from "./_lib/oauth2";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import { IncomingMessage, ServerResponse, createServer } from "http";
+
+
+import simpleOauthModule from "simple-oauth2";
+
+const create = () =>
+  simpleOauthModule.create({
+    client: {
+      id: process.env.OAUTH_CLIENT_ID,
+      secret: process.env.OAUTH_CLIENT_SECRET
+    },
+    auth: {
+      tokenHost: `https://github.com`,
+      tokenPath: `/login/oauth/access_token`,
+      authorizePath: `/login/oauth/authorize`
+    }
+  });
+
+type RenderBody = {
+  (status: "success", content: { token: string; provider: "github" }): string;
+  (status: "error", content: Object): string;
+};
+const renderBody: RenderBody = (status, content) => `
+<script>
+  const receiveMessage = (message) => {
+    window.opener.postMessage(
+      'authorization:github:${status}:${JSON.stringify(content)}',
+      message.origin
+    );
+
+    window.removeEventListener("message", receiveMessage, false);
+  }
+  window.addEventListener("message", receiveMessage, false);
+  
+  window.opener.postMessage("authorizing:github", "*");
+</script>
+`;
+
+
+
+
 
 dotenv.config();
 
